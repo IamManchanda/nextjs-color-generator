a09416dc6b390b8e0cc476eca324597e20af589a
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-13 19:29:03 +0530

    SingleColor Component

---
+++ src/components/single-color.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hexValue = `#${hexColor}`;
+
+  function copyToClipboard() {
+    setAlert(true);
+    navigator.clipboard.writeText(hexValue);
+  }
+
+  function clearAlert() {
+    setAlert(false);
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(clearAlert, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={copyToClipboard}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
